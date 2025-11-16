3418404995ad235b7da95243906d15abc9798812
Marcos Rodrigues <marcos.mlr@gmail.com>
2025-11-16 20:21:53 -0300

    Fix: invalid value encountered in divide ndvi = (NIR - Red)/(NIR + Red)

--- src/cube4health/eddpr/eddpr.py
+++ src/cube4health/eddpr/eddpr.py
@@ -422,6 +422,9 @@ def calc_ndvi(out_fname, fname):
         
         NIR = arr[bands['NIR'],...]
         Red = arr[bands['Red'],...]
+        with np.errstate(divide='ignore', invalid='ignore'):
+            ndvi = (NIR - Red) / (NIR + Red)
+            ndvi[~np.isfinite(ndvi)] = np.nan
         ndvi = (NIR - Red)/(NIR + Red)
         ndvi = np.where((ndvi<-1.),-1.,ndvi) #remove outliers
         ndvi = np.where((ndvi>1.),1.,ndvi) #remove outliers
