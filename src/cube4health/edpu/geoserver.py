aea96a424397d085317d9bb58ead5ac87781e785
Marcos Rodrigues <marcos.mlr@gmail.com>
2026-03-16 11:47:36 -0300

    Change the Geoserver class from the EDPU to show the exception when trying to connect at Geoserver. Close #39

--- src/cube4health/edpu/geoserver.py
+++ src/cube4health/edpu/geoserver.py
@@ -137,12 +137,13 @@ class GeoServer:
                                    password=self._password)
 
                 # Verifies if the geoserver password informed is correct
-                status_geoserver = self.geoserver.get_status()
+                status_geoserver = self.geoserver.get_status()                
                 if status_geoserver:
                     if config.geoserver_passwd == None:
                         config.geoserver_passwd = self._password
                     break
-            except:
+            except Exception as e:
+                print(e)
                 print(f'\ntry {try_connect}/3\n')
         else:
                 print('-'*120)
